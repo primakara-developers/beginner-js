49b4e67cd457035f06fb6dd12616b4f315864bf9
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2023-12-22 18:39:56 +0800

    feat: implement more simple add todo logic, implement delete todo logic

--- meet-7/script.js
+++ meet-7/script.js
@@ -1,5 +1,26 @@
+const todos = [];
+
+function displayTodos() {
+  // Select the task list ul element
+  const todoList = document.getElementById('todo-list');
+
+  // Clear the existing list
+  todoList.innerHTML = '';
+
+  for (i = 0; i < todos.length; i++) {
+    todoList.innerHTML += `
+    <li>
+    ${todos[i]}
+    <button onclick="deleteTodo(${i})">X</button>
+    </li>
+    `;
+  }
+}
+
 function addTodo() {
+  // Select the input element
   const todoInput = document.getElementById('todo-input');
+
   // Get the value of user input from input field
   const todoData = todoInput.value;
 
@@ -8,14 +29,22 @@ function addTodo() {
     alert('Input masih kosong!');
   }
 
-  // Create a new list item
-  var newTodo = document.createElement('li');
-  newTodo.appendChild(document.createTextNode(todoData));
-
-  // Add the new task to the task list
-  const todoList = document.getElementById('todo-list');
-  todoList.appendChild(newTodo);
+  // Input new todo data to "todos" array
+  todos.push(todoData);
 
   // Clear the input field value
   todoInput.value = '';
+
+  // Trigger display function for update new data
+  displayTodos();
 }
+
+function deleteTodo(index) {
+  // Remove the task at the specified index
+  todos.splice(index, 1);
+
+  // Refresh the task list
+  displayTodos();
+}
+
+displayTodos();
